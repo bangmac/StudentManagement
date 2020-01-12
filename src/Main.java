c164a038fa7280b5794e1673faf3549966d27ddd
bmv <leeloo1899@gmail.com>
2020-01-12 23:07:31 +0700

    done

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static Student [] studentsList = new Student[1];
+    public static void main(String[] args) {
+        studentsList[0] = new Student(1,"PEPEG","OMEGALUL","0846546556465",5);
+        Manager admin = new Manager();
+        admin.add(new Student(2,"KEKW","OMEGALUL","09845664",10));
+        admin.add(new Student(3,"KEKW","OMEGALUL","09845664",2));
+        System.out.println("Number of Student: "+studentsList.length);
+        admin.deleteStudent(1);
+        System.out.println("Number of Student: "+studentsList.length);
+        System.out.println("Average Score: "+admin.getAverageGradeOfStudents());
+    }
+
+}
