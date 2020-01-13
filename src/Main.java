e29e8ab1b693286a288e7bd66b19c6de531d7e93
bmv <leeloo1899@gmail.com>
2020-01-13 10:01:05 +0700

    done

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static Student [] studentsList = new Student[1];
     public static void main(String[] args) {
-        studentsList[0] = new Student(1,"PEPEG","OMEGALUL","0846546556465",5);
+        studentsList[0] = new Student(1,"PEPEG","XYZ","12345678910",5);
         Manager admin = new Manager();
-        admin.add(new Student(2,"KEKW","OMEGALUL","09845664",10));
-        admin.add(new Student(3,"KEKW","OMEGALUL","09845664",2));
+        admin.add(new Student(2,"ABC","XYZ","99999999",10));
+        admin.add(new Student(3,"ABC","XYZ","99999999",2));
         System.out.println("Number of Student: "+studentsList.length);
         admin.deleteStudent(1);
         System.out.println("Number of Student: "+studentsList.length);
